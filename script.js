8dbc6a6ae2d416a0265797a748b5de2217ad37d3
shireenzd <shireenzeineddine@gmail.com>
2023-12-04 15:31:25 +0200

    filtered

--- script.js
+++ script.js
@@ -1,32 +1,17 @@
-let notesContainer = document.querySelector('.notes-container');
-let notes = [
-  {
-    id: '1',
-    content: 'random text 1',
-    priority: 1,
-    category: 'home',
-    profile: 'images/5987424.png'
-  },
-  {
-    id: '2',
-    content: 'random text 2',
-    priority: 1,
-    category: 'hobbies',
-    profile: 'images/5987424.png'
-  },
-  {
-    id: '3',
-    content: 'random text 3',
-    priority: 1,
-    category: 'work',
-    profile: 'images/5987424.png'
-  }
-];
+let categoryToClassesMap = {
+  home:"bg-red-200",
+  hobbies:"bg-green-200",
+  work:"bg-yellow-200"
+};
 
-updateUINotesContainer();
-function updateUINotesContainer() {
+let notesContainer = document.querySelector('.notes-container');
+let notes = localStorage.getItem('notes') ? JSON.parse(localStorage.getItem('notes')) : [];
+
+updateNotesArray(notes)
+function updateUINotesContainer(notesToDisplay = null) {
+  notesToDisplay = notesToDisplay || notes
   notesContainer.innerHTML = ''
-  notes.forEach(function (noteItem) {
+  notesToDisplay.forEach(function (noteItem) {
     let noteHTML = generatNoteHTML(noteItem);
     notesContainer.innerHTML += noteHTML;
   });
@@ -34,18 +19,25 @@ function updateUINotesContainer() {
 
 function generatNoteHTML(noteItem) {
   return `
-  <div class="flex items-center gap-5 p-4 rounded-md bg-gray-200 relative">
+  <div class="flex items-center ${categoryToClassesMap[noteItem.category]} gap-5 p-4 rounded-md bg-gray-200 relative">
       <div class="h-12 w-12 overflow-hidden rounded-full">
           <img src="${noteItem.profile}" alt="" />
       </div>
       <p>${noteItem.content}</p>
-      <p class="absolute top-4 right-4 rounded-full h-8 w-8 p-3 bg-white flex justify-center items-center cursor-pointer"><span onclick="deleteNote('${noteItem.id}')">X</span></p>
+      <div class="actions absolute top-4 right-4 flex gap-3">
+        <p class="rounded-full h-8 w-8 p-3 bg-white flex justify-center items-center cursor-pointer">
+          <span onclick="editNote('${noteItem.id}')"> edit </span>
+        </p>
+        <p class="rounded-full h-8 w-8 p-3 bg-white flex justify-center items-center cursor-pointer">
+          <span onclick="deleteNote('${noteItem.id}')">X</span>
+        </p>
+      </div>
   </div>
   `;
 }
 
 function deleteNote(noteID){
-  notes = notes.filter(function(note){
+  let newNotes = notes.filter(function(note){
     // if same ID that we want to delete, return false
     if(noteID === note.id){
       return false;
@@ -53,13 +45,13 @@ function deleteNote(noteID){
       return true;
     }
   })
-  updateUINotesContainer();
+  updateNotesArray(newNotes)
 }
 
 let textAreaInput = document.querySelector('textarea')
 let textAreaInputError = document.querySelector('#text-area-error')
 
-let categoryInput = document.querySelector('select')
+let categoryInput = document.querySelector('#category')
 let categoryInputError = document.querySelector('#category-input-error')
 
 function addNote(){
@@ -70,7 +62,7 @@ function addNote(){
   if(contentValid &&  priorityValid && categoryValid){
     let priorityInputValue = document.querySelector('input[name="priority"]:checked').value
     let newNote = {
-      id : generateNoteID(),
+      id : noteBeingEdited?.id ? noteBeingEdited.id : generateNoteID(),
       profile: 'profile.webp'
     };
     newNote.content = textAreaInput.value
@@ -78,8 +70,8 @@ function addNote(){
     newNote['category'] = categoryInput.value
     
     // push to notes array
-    notes.push(newNote)
-    updateUINotesContainer();
+    updateNotesArray([...notes, newNote]);
+    populateFormInputs();
   }
   
 }
@@ -116,4 +108,71 @@ function validateCategoryInput(){
   }
   categoryInputError.classList.add('hidden')
   return true;
+}
+
+let noteBeingEdited;
+function editNote (noteID){
+  // get note
+  noteBeingEdited = notes.find(function(note){
+    return noteID === note.id
+  })
+  if(!noteBeingEdited){
+    return;
+  }
+  
+  // remove from notes array
+  deleteNote(noteID)
+  
+  // populate in inputs
+  populateFormInputs(noteBeingEdited)
+}
+
+function populateFormInputs(noteObject = {}){
+  // content
+  textAreaInput.value = noteObject.content || '';
+  // priority
+  // document.querySelectorAll('input[type=radio]')[4].value
+  document.querySelectorAll('input[name="priority"]').forEach(function(radioInput){
+    if(radioInput.value == noteObject.priority){
+      radioInput.checked = true;
+    } else {
+      radioInput.checked = false;
+    }
+  })
+  
+  // category
+  categoryInput.value = noteObject.category || ''
+  
+}
+
+function updateNotesArray (notesArray=[]){
+  notes = notesArray
+  localStorage.setItem('notes', JSON.stringify(notes))
+  // reflect changes to UI
+  // never call outside of this function
+  updateUINotesContainer();
+}
+
+function sortNotesAsc(){
+  let sortedNotesArray = notes.sort(function(a,b){
+    return a.priority-b.priority
+  })
+  updateNotesArray(sortedNotesArray)
+}
+function sortNotesDesc(){
+  let sortedNotesArray = notes.sort(function(a,b){
+    return b.priority-a.priority
+  })
+  updateNotesArray(sortedNotesArray)
+}
+
+function filterNotesByCategory(){
+  let filteredArray = notes.filter(function(noteItem){
+    if(!this.event.target.value || noteItem.category == this.event.target.value){
+      return true;
+    }
+    return false;
+  })
+  
+  updateUINotesContainer(filteredArray)
 }
